96a545191d2c7f197f6afb7455d25ac2f76264e9
Alix Axel <alix.axel@gmail.com>
2012-11-22 18:24:40 -0500

    fixed bug

--- halBox/php/var/www/html/adminer/index.php
+++ halBox/php/var/www/html/adminer/index.php
@@ -4,18 +4,21 @@ if (is_file('./adminer.php') === true)
 {
 	if ((is_dir('./plugins/') === true) && (is_file('./plugins/plugin.php') === true))
 	{
-		include_once('./plugins/plugin.php');
-
-		foreach (glob('./plugins/*.php') as $plugin)
+		function adminer_object()
 		{
-			include_once($plugin);
+			include_once('./plugins/plugin.php');
+
+			foreach (glob('./plugins/*.php') as $plugin)
+			{
+				include_once($plugin);
+			}
+
+			$plugins = array
+			(
+			);
+
+			return new AdminerPlugin($plugins);
 		}
-
-		$plugins = array
-		(
-		);
-
-		return new AdminerPlugin($plugins);
 	}
 
 	include_once('./adminer.php');
