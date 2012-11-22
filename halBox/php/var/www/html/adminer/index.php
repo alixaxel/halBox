6a8783c01490e2526471f09a093e349716a7dfc2
Alix Axel <alix.axel@gmail.com>
2012-11-22 17:56:04 -0500

    fixed bug

--- halBox/php/var/www/html/adminer/index.php
+++ halBox/php/var/www/html/adminer/index.php
@@ -4,6 +4,8 @@ if (is_file('./adminer.php') === true)
 {
 	if ((is_dir('./plugins/') === true) && (is_file('./plugins/plugin.php') === true))
 	{
+		include_once('./plugins/plugin.php');
+
 		foreach (glob('./plugins/*.php') as $plugin)
 		{
 			include_once($plugin);
