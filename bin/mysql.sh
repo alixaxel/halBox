e79b01490d9480a3da4abfdc5f8d857d29249853
Alix Axel <alix.axel@gmail.com>
2013-04-04 13:38:50 -0400

    just testing MySQL remote access

--- bin/mysql.sh
+++ bin/mysql.sh
@@ -12,7 +12,7 @@ expect "Enter current password for root (enter for none):" { send -- "\r";
             expect "New password:" { send -- "$password\r";
                 expect "Re-enter new password:" { send -- "$password\n";
                     expect "Remove anonymous users" { send -- "y\r";
-                        expect "Disallow root login remotely" { send -- "y\r";
+                        expect "Disallow root login remotely" { send -- "n\r";
                             expect "Remove test database and access to it" { send -- "y\r";
                                 expect "Reload privilege tables now" { send -- "y\r";
                                     expect "Thanks for using MySQL!" {
--- halBox.sh
+++ halBox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-clear && echo -e "\e[1;31mhalBox 0.26.2\e[0m\n"
+clear && echo -e "\e[1;31mhalBox 0.26.3\e[0m\n"
 
 if [[ $( whoami ) != "root" ]]; then
     echo -e "\e[1;31mDave, is that you?\e[0m" && exit 1
